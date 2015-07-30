841525f43a5b3638ddeb21679577c910498e9fa8
Nick Catalano <nickcatal@gmail.com>
2015-07-30 00:04:09 -0500

    Removing unnecessary middleware files

--- middleware/middleware.py
+++
@@ -1,7 +0,0 @@
-from django.utils.cache import add_never_cache_headers
-
-class DisableStaffCachingMiddleware(object):
-    def process_response(self, request, response):
-        if hasattr(request, 'user') and request.user.is_staff:
-            add_never_cache_headers(response)
-        return response
